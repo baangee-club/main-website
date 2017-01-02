bbc7689d3074cc103216839ea0346238be58a9d2
Zeeshan Khan <zkhan19093@gmail.com>
2017-01-02 11:14:46 +0530

    update django version

--- wsgi/baangeeclub/baangeeclub/urls.py
+++ wsgi/baangeeclub/baangeeclub/urls.py
@@ -1,4 +1,4 @@
-from django.conf.urls import patterns, include, url
+from django.conf.urls import url, include
 from django.contrib import admin
 from django.conf import settings
 from django.conf.urls.static import static
@@ -8,9 +8,11 @@ handler500 = 'home_baangee.views.handler500'
 handler403 = 'home_baangee.views.handler403'
 handler400 = 'home_baangee.views.handler400'
 
-urlpatterns = patterns('',
+from django.contrib import admin
+admin.autodiscover()
+
+urlpatterns = [
     # Examples:
     # url(r'^blog/', include('blog.urls')),
     url(r'^',include('home_baangee.urls')),
-    url(r'^admin/', include(admin.site.urls)),
-) + static(settings.MEDIA_ROOT, document_root=settings.MEDIA_URL)
+]+ static(settings.MEDIA_ROOT, document_root=settings.MEDIA_URL)
